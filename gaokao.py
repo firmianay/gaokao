cc82bf177ed72c6631efea9fcd879b1f551a73db
firmianay <firmianay@gmail.com>
2017-06-26 10:22:02 +0800

    small fix

--- gaokao.py
+++ gaokao.py
@@ -1,6 +1,7 @@
 # -*- coding: utf_8 -*-
 
 import config
+import json
 from itertools import islice
 
 class ZhiYuan():
@@ -11,14 +12,14 @@ class ZhiYuan():
         self.score = config.get('score')
         self.rank = config.get('rank')
         self.preScore = {
-            'score_1':'',
-            'score_2':'',
-            'score_3':''
+            '2016':'',
+            '2015':'',
+            '2014':''
         }
         self.preRank = {
-            'rank_1':'',
-            'rank_2':'',
-            'rank_3':''
+            '2016':'',
+            '2015':'',
+            '2014':''
         }
         self.majors_1 = {}
         self.majors_2 = {}
@@ -29,56 +30,56 @@ class ZhiYuan():
 
     def generate(self):
 
-        with open('./2016tj.txt', 'r') as f:
+        with open('./data/2016tj.txt', 'r') as f:
             for line in islice(f, 1, None):
                 line = line.split()
                 if self.rank > int(line[4])-int(line[3]) and self.rank < int(line[4]):
-                    self.preScore['score_1'] = line[0]
+                    self.preScore['2016'] = line[0]
                     if self.subject == '理科':
-                        self.preRank['rank_1'] = "%s~%s" % (int(line[4])-int(line[3]), line[4])
+                        self.preRank['2016'] = "%s~%s" % (int(line[4])-int(line[3]), line[4])
                     elif self.subject == '文科':
-                        self.preRank['rank_1'] = "%s~%s" % (int(line[2])-int(line[1]), line[2])
+                        self.preRank['2016'] = "%s~%s" % (int(line[2])-int(line[1]), line[2])
 
-        with open('./2015tj.txt', 'r') as f:
+        with open('./data/2015tj.txt', 'r') as f:
             for line in islice(f, 1, None):
                 line = line.split()
                 if self.rank > int(line[4])-int(line[3]) and self.rank < int(line[4]):
-                    self.preScore['score_2'] = line[0]
+                    self.preScore['2015'] = line[0]
                     if self.subject == '理科':
-                        self.preRank['rank_2'] = "%s~%s" % (int(line[4])-int(line[3]), line[4])
+                        self.preRank['2015'] = "%s~%s" % (int(line[4])-int(line[3]), line[4])
                     elif self.subject == '文科':
-                        self.preRank['rank_2'] = "%s~%s" % (int(line[2])-int(line[1]), line[2])
+                        self.preRank['2015'] = "%s~%s" % (int(line[2])-int(line[1]), line[2])
 
-        with open('./2014tj.txt', 'r') as f:
+        with open('./data/2014tj.txt', 'r') as f:
             for line in islice(f, 1, None):
                 line = line.split()
                 if self.rank > int(line[4])-int(line[3]) and self.rank < int(line[4]):
-                    self.preScore['score_3'] = line[0]
+                    self.preScore['2014'] = line[0]
                     if self.subject == '理科':
-                        self.preRank['rank_3'] = "%s~%s" % (int(line[4])-int(line[3]), line[4])
+                        self.preRank['2014'] = "%s~%s" % (int(line[4])-int(line[3]), line[4])
                     elif self.subject == '文科':
-                        self.preRank['rank_3'] = "%s~%s" % (int(line[2])-int(line[1]), line[2])
+                        self.preRank['2014'] = "%s~%s" % (int(line[2])-int(line[1]), line[2])
 
-        with open('./2016zy.txt', 'r')as f:
+        with open('./data/2016zy.txt', 'r')as f:
             for line in islice(f, 1, None):
                 line = line.split()
-                if int(self.preScore['score_1'].split("~")[1]) >= int(line[1]):
+                if int(self.preScore['2016'].split("~")[1]) >= int(line[1]):
                     self.majors_1[line[0]] = 1
                 else:
                     self.majors_1[line[0]] = 0
 
-        with open('./2015zy.txt', 'r')as f:
+        with open('./data/2015zy.txt', 'r')as f:
             for line in islice(f, 1, None):
                 line = line.split()
-                if int(self.preScore['score_2'].split("~")[1]) >= int(line[1]):
+                if int(self.preScore['2015'].split("~")[1]) >= int(line[1]):
                     self.majors_2[line[0]] = 1
                 else:
                     self.majors_2[line[0]] = 0
 
-        with open('./2014zy.txt', 'r')as f:
+        with open('./data/2014zy.txt', 'r')as f:
             for line in islice(f, 1, None):
                 line = line.split()
-                if int(self.preScore['score_3'].split("~")[1]) >= int(line[1]):
+                if int(self.preScore['2014'].split("~")[1]) >= int(line[1]):
                     self.majors_3[line[0]] = 1
                 else:
                     self.majors_3[line[0]] = 0
@@ -146,7 +147,7 @@ class ZhiYuan():
             '2014录取情况':self.majors_3,
             '2017录取预测':self.probability
         }
-        print(obj)
+        print(json.dumps(obj, indent=4, ensure_ascii=False))
 
     def start(self):
         self.generate()
